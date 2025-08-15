1c577f2704eb3859de5d8b95df2604ec21f2c9c9
villaclara <villaclara1231@gmail.com>
2025-08-15 14:21:25 +0300

    a

--- BankSystem.Services/Models/BankAccountFullInfoModel.cs
+++ BankSystem.Services/Models/BankAccountFullInfoModel.cs
@@ -1,1 +1,12 @@
 namespace BankSystem.Services.Models;
+
+public class BankAccountFullInfoModel
+{
+    public int BankAccountId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string AccountNumber { get; set; }
+    public decimal Balance { get; set; }
+    public string CurrencyCode { get; set; }
+    public int BonusPoints { get; set; }
+}
--- BankSystem.Services/Services/AccountService.cs
+++ BankSystem.Services/Services/AccountService.cs
@@ -1,1 +1,10 @@
+using BankSystem.Services.Models;
+
 namespace BankSystem.Services.Services;
+public class AccountService
+{
+    public IReadOnlyList<BankAccountFullInfoModel> GetBankAccountsFullInfo()
+    {
+        throw new NotImplementedException();
+    }
+}
