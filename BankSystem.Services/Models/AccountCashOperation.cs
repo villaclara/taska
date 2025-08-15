e8f34c322a15e59f805d937872773fcd240cfe6b
villaclaraaa <abbywinstone1231@gmail.com>
2025-08-15 12:54:34 +0300

    asfafsf

--- BankSystem.Services/Models/AccountCashOperation.cs
+++ BankSystem.Services/Models/AccountCashOperation.cs
@@ -1,1 +1,22 @@
 namespace BankSystem.Services.Models;
+
+public class AccountCashOperation
+{
+    public decimal Amount { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Note { get; set; }
+
+    public AccountCashOperation(decimal amount, DateTime date, string note)
+    {
+        this.Amount = amount;
+        this.Date = date;
+        this.Note = note;
+    }
+
+    public override string ToString()
+    {
+        throw new NotImplementedException();
+    }
+}
--- BankSystem.Services/Models/Accounts/BankAccount.cs
+++ BankSystem.Services/Models/Accounts/BankAccount.cs
@@ -1,1 +1,15 @@
 namespace BankSystem.Services.Models.Accounts;
+
+public class BankAccount
+{
+    public string Number { get; set; }
+    public string Balance { get; set; }
+
+    public string CurrencyCode { get; set; }
+    public AccountOwner Owner { get; set; }
+    public int BonusPoints { get; set; }
+    public decimal OverDraft { get; set; }
+
+
+}
+
