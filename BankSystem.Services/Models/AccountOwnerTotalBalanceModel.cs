bc57bb5aa65a672dd7dbe6a8259a06a52db342be
villaclaraaa <abbywinstone1231@gmail.com>
2025-08-15 14:21:44 +0300

    a

--- BankSystem.Services/Models/AccountOwnerTotalBalanceModel.cs
+++ BankSystem.Services/Models/AccountOwnerTotalBalanceModel.cs
@@ -1,1 +1,11 @@
 namespace BankSystem.Services.Models;
+
+public class AccountOwnerTotalBalanceModel
+{
+    public int AccountOwner { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string CurrencyCode { get; set; }
+    public decimal Total { get; set; }
+}
+
--- BankSystem.Services/Services/AccountService.cs
+++ BankSystem.Services/Services/AccountService.cs
@@ -1,1 +1,12 @@
+using BankSystem.EF.Entities;
+using BankSystem.Services.Models;
+
 namespace BankSystem.Services.Services;
+
+public class OwnerService
+{
+    public IReadOnlyList<AccountOwnerTotalBalanceModel> GetAccountOwnersTotalBalance()
+    {
+        throw new NotImplementedException();
+    }
+}
