700cc30369a4bbcbb6738b66d9ce18bad7409df6
villaclaraaa <abbywinstone1231@gmail.com>
2025-08-15 14:24:08 +0300

    a

--- BankSystem.Services/Services/OwnerService.cs
+++ BankSystem.Services/Services/OwnerService.cs
@@ -1,1 +1,10 @@
+using BankSystem.Services.Models;
 namespace BankSystem.Services.Services;
+
+public class OwnerService
+{
+    public IReadOnlyList<AccountOwnerTotalBalanceModel> GetAccountOwnersTotalBalance()
+    {
+        throw new NotImplementedException();
+    }
+}
